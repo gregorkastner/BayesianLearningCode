7e75e20da51caecf4716d83ea458e57e142d67c5
Gregor Kastner <gregor.kastner@aau.at>
2025-03-03 20:29:10 +0100

    minor adaptations for data

--- R/data.R
+++ R/data.R
@@ -3,8 +3,7 @@
 #' Seriously injured or killed pedestrians per month in Linz, Austria, from
 #' January 1987 to December 2002.
 #'
-#' @format ## `accidents`
-#' A monthly time series object with 192 rows and 4 columns
+#' @format A monthly time series object with 192 rows and 4 columns
 #' \describe{
 #'   \item{children}{number of children aged 6 to 10 years seriously injured or killed}
 #'   \item{seniors}{senior persons above 65 seriously injured or killed}
