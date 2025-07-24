9c028825d079f64115ee19350cc50fb03d5922a4
Gregor Kastner <gregor.kastner@aau.at>
2025-07-24 23:49:51 +0200

    adds US GDP data

--- R/data.R
+++ R/data.R
@@ -1,3 +1,18 @@
+#' GDP data
+#'
+#' Gross domestic product of the USA. Quarterly data from 1947Q1 to 2025Q1.
+#'
+#' Gross domestic product (GDP), the featured measure of U.S. output,
+#' is the market value of the goods and services produced by labor and property
+#' located in the United States. The units are Billions of Dollars,
+#' Seasonally Adjusted Annual Rate.
+#'
+#' @format A sequence of length 313
+#' @source U.S. Bureau of Economic Analysis, Gross Domestic Product [GDP],
+#' retrieved from FRED, Federal Reserve Bank of St. Louis;
+#' \url{https://fred.stlouisfed.org/series/GDP}, July 24, 2025.
+"gdp"
+
 #' Labor Market Data
 #'
 #' Labor market data of Austrian employees. The data stem from 1986--1998.
